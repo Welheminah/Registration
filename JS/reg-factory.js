51cf22e5fdbeb637c111144fe130ae2c0e15f70e
Welheminah <ponelehutso@gmail.com>
2021-06-02 12:01:12 +0200

    ckeckin

--- JS/reg-factory.js
+++ JS/reg-factory.js
@@ -7,19 +7,30 @@ function regPlate() {
 
     
     function numberPlates(registration){
+        var registration = registration.trim().toUpperCase();
         var type =  /^((CJ|CY|CL|CA)\-([0-9]){3}\-([0-9]){3})$/;
         var type2 = /^((CJ|CY|CL|CA)\s([0-9]){3}\s([0-9]){3})$/;
         var type3 = /^(CJ|CY|CL|CA)\-[0-9]{5}$/;
+        var type4 = /^(CJ|CY|CL|CA)\s[0-9]{5}$/;
         var isValid = type.test(registration);
         var isValid2 = type2.test(registration);
         var isValid3 = type3.test(registration);
-        // alert(isValid)
-        if(isValid || isValid2 || isValid3){
+        var isValid4 = type4.test(registration);
+
+        if(isValid || isValid2 || isValid3 || isValid4){
+            if(!platesStore.includes(registration)){
+                platesStore.push(registration)
+                console.log(platesStore);
+                return;
+            } 
+            else {
+                return registration;
+            }
             
             
-            return registration
         }
         else {
+
             return false
         }
 
