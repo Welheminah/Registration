a3753b050d48fd65facbd56e73e8a8116b347211
Welheminah <ponelehutso@gmail.com>
2021-06-08 15:47:18 +0200

    push

--- JS/dom-logic.js
+++ JS/dom-logic.js
@@ -2,34 +2,58 @@ var theButton = document.querySelector(".theButton");
 var regNo = document.querySelector(".regNo");
 var dispayElement = document.querySelector(".dispay");
 var alertElement = document.querySelector(".alert");
+var select = document.querySelector(".select");
+var show = document.querySelector(".show");
+var dispayFilter = document.querySelector(".dispayFilter");
+
 
 var storePlates = [];
+var radio = document.querySelector("input[name='town']:checked");
 
 
 var regInstance = regPlate();
 
 function theReg() {
-  // regInstance.numberPlates(regNo.value)
-
-
-
-  var newElement = document.createElement('button');
+var newElement = document.createElement('button');
+  newElement.classList.add("btn");
 
   // var innerT = document.createTextNode(regNo.value);
   //var innerT = document.createTextNode(storePlates[i]);
   if (regInstance.numberPlates(regNo.value) !== false) {
-    newElement.innerText = regInstance.numberPlates(regNo.value)
-    dispayElement.appendChild(newElement);
+    newElement.innerText = regInstance.numberPlates(regNo.value);
+    dispayElement.appendChild(newElement)
   }
-  // else {
-  //   alertElement.innerHTML = "Enter valid registration";
-  // }
-
-
+  regNo.value = " ";
 }
 
+function goFilter(){
+  var radio = document.querySelector("input[name='town']:checked");
+  var radioBtn = radio.value;
+  
+  if(radioBtn){
+    
+    var regFilter = regInstance.filterReg(radioBtn)
+    console.log(regFilter);
+   for (i = 0; i < regFilter.length; i++) {
+  var newElement = document.createElement('button');
 
-regNo.value = " ";
+
+      newElement.classList.add("btn");
+       newElement.innerText = regFilter[i];
+       dispayFilter.appendChild(newElement);
+   }
+      }
+    
+    }
+function letsUncheck(){
+  var radio = document.querySelector("input[name='town']:checked");
+    radio.checked = false;
+  }    
+
+show.addEventListener('click', goFilter);
+
+
 
 
 theButton.addEventListener('click', theReg);
+show.addEventListener('click', letsUncheck)
--- JS/reg-factory.js
+++ JS/reg-factory.js
@@ -1,79 +1,82 @@
 function regPlate() {
 
-    var platesStore = [];
+    var platesStore = [] ;
 
     //The format must be created so that the reg dont take anything
     
-
+    
     
     function numberPlates(registration){
         var registration = registration.trim().toUpperCase();
-        var type =  /^((CJ|CY|CL|CA)\-([0-9]){3}\-([0-9]){3})$/;
-        var type2 = /^((CJ|CY|CL|CA)\s([0-9]){3}\s([0-9]){3})$/;
-        var type3 = /^(CJ|CY|CL|CA)\-[0-9]{5}$/;
-        var type4 = /^(CJ|CY|CL|CA)\s[0-9]{5}$/;
-        var isValid = type.test(registration);
-        var isValid2 = type2.test(registration);
-        var isValid3 = type3.test(registration);
-        var isValid4 = type4.test(registration);
+        var type =  /^((CJ|CL|CA)\-([0-9]){3}\-([0-9]){3})$/;
+        var type2 = /^((CJ|CL|CA)\s([0-9]){3}\s([0-9]){3})$/;
+        var type3 = /^(CJ|CL|CA)\-[0-9]{5}$/;
+        var type4 = /^(CJ|CL|CA)\s[0-9]{5}$/;
+        var letsValidate = type.test(registration);
+        var letsValidate2 = type2.test(registration);
+        var letsValidate3 = type3.test(registration);
+        var letsValidate4 = type4.test(registration);
 
-        if(isValid || isValid2 || isValid3 || isValid4){
+        if(letsValidate || letsValidate2 || letsValidate3 || letsValidate4){
             if(!platesStore.includes(registration)){
-                platesStore.push(registration)
-               
+                platesStore.push(registration);
+                console.log(platesStore);
                 return;
             } 
             else {
                 return registration;
             }
-            
-            
+            // return registration
         }
         else {
-
             return false
-        }
-
-        
-
-        
-
-    //    if(type.test(registration)){
-           
-    //     platesStore.push(registration);
-    //     alert("correct");
-        
-    //    }
-    //    else {
-           
-    //    }
-    //    console.log(registration);
-    //    console.log(platesStore);
-        //    return platesStore;
+        } 
     }
 
-    // function getReg(){
-    //     return weStoreReg();
-    // }
 
     function letsFilter(){
-
+        if(registration.startsWith("CA")){
+            return cpt;
+        }
+        else if(registration.startsWith("CJ")){
+            return paarl;
+        }
+        else if(registration.startsWith("CL")){
+            return stellenbosch;
+        }
+        else if(registration.startsWith("CA", "CJ", "CL")){
+            return all;
+        }
     }
 
-    function storings(){
-        if(!platesStore.includes(registration)){
-            platesStore.push(registration)
+    // function storings(registration){
+    //     if(!platesStore.includes(registration)){
+    //             platesStore.push(registration);
+    //             console.log(platesStore);            
+    //             return;
+    //         } 
+    //         else {
+    //             return registration;
+    //         }
+    // }
+
+    function filterReg(radioBtn){
+        var filteredReg = [];
+            for (i = 0; i < platesStore.length; i++) {
+                if(platesStore[i].startsWith(radioBtn)){
+                    filteredReg.push(platesStore[i]);
+            }
+
         }
-        else {
-            return;
-        }
+        return filteredReg;
+
     }
+    
 
 
     return {
         numberPlates,
         letsFilter,
-        storings
-        // getReg
+        filterReg,
     }
 }
