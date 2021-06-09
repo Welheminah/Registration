8200e4c0ab7c6061a8dfe595cf6235b600c544e8
Welheminah <ponelehutso@gmail.com>
2021-06-09 10:49:37 +0200

    platesStore

--- Tests/JS/reg.test.js
+++ Tests/JS/reg.test.js
@@ -33,3 +33,21 @@ it('should be to accept the registration nummber specified in the regex', functi
         });
 
 });
+describe('The plateStore function', function(){
+
+    it('it should return the names greeted', function(){
+
+        let newReg = regPlate();
+
+        newReg.returnArray("CJ-12345");
+        newReg.returnArray("CJ-123-123");
+        newReg.returnArray("CA 123 123");
+        newReg.returnArray("CA 123 123");
+    
+    
+
+
+
+    assert.deepEqual(["CJ-12345", "CJ-123-123", "CA 123 123"], newReg.returnArray(3))
+    });
+});    
