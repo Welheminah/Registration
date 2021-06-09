4b7807ae7c712ee9610b20ab6e8f38a1e383c14f
Welheminah <ponelehutso@gmail.com>
2021-06-09 10:17:21 +0200

    test

--- Tests/reg.test.js
+++ Tests/reg.test.js
@@ -13,7 +13,23 @@ describe('The Registration function', function(){
         let newReg = regPlate();
         newReg.numberPlates('CL 12345');
     
-        assert.equal('CA 123 123', newReg.returnArray('CA 123 123'))
+        assert.equal('CL 12345', newReg.returnArray('CL 12345'))
+        });
+
+    it('should be to accept the registration nummber specified in the regex', function(){
+
+        let newReg = regPlate();
+        newReg.numberPlates('CJ-123-123');
+
+        assert.equal('CJ-123-123', newReg.returnArray('CJ-123-123'))
         });
     
+it('should be to accept the registration nummber specified in the regex', function(){
+
+        let newReg = regPlate();
+        newReg.numberPlates('CJ-12345');
+
+        assert.equal('CJ-12345', newReg.returnArray('CJ-12345'))
+        });
+
 });
