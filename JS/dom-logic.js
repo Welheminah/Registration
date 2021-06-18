8224901d8b26049ee82c8e5a874a02892957fdea
Welheminah <ponelehutso@gmail.com>
2021-06-18 14:16:37 +0200

    doneee

--- JS/dom-logic.js
+++ JS/dom-logic.js
@@ -33,10 +33,10 @@ function theReg() {
   var newElement = document.createElement('button');
   newElement.classList.add("btn");
   var newElement2 = document.createElement('p');
-
+  
 
   var ourValue = regInstance.numberPlates(regNo.value);
- 
+
 
   if (ourValue === "Invalid regitration, please enter a valid registration eg CA 12345") {
     regNo.value = " ";
@@ -54,13 +54,13 @@ function theReg() {
     newElement2.classList.add("errorMessages")
     alertElement.appendChild(newElement2);
     alertElement.innerHTML = "The registration entered already exist";
-    setTimeout(function () { 
+    setTimeout(function () {
       alertElement.innerText = ''
     }, 4000);
     return;
-  
-  } 
-  
+
+  }
+
 
   newElement.innerText = ourValue;
   dispayElement.appendChild(newElement);
@@ -79,30 +79,47 @@ function goFilter() {
 
   var radio = document.querySelector("input[name='town']:checked");
   var radioBtn = radio.value;
+  var beacon = regInstance.filterReg(radioBtn);
+  var newElement2 = document.createElement('p');
 
 
   if (radioBtn) {
 
     for (i = 0; i < storePlatesLocal.length; i++) {
+
+
       if (storePlatesLocal[i].startsWith(radioBtn)) {
+        console.log(storePlatesLocal[i])
         var newElement = document.createElement('button');
         newElement.classList.add("btn");
         newElement.innerText = storePlatesLocal[i];
         dispayFilter.appendChild(newElement);
-      }
-      else if(radioBtn === "ALL"){
+      } else if (radioBtn === "ALL") {
         var newElement = document.createElement('button');
         newElement.classList.add("btn");
         newElement.innerText = storePlatesLocal[i];
         dispayElement.appendChild(newElement);
       }
+
+      if(beacon.length === 0) {
+
+        newElement2.classList.add("notown")
+        dispayFilter.style.fontSize = "1.3rem";
+        dispayFilter.style.color = "#2F4F4F";
+        dispayFilter.appendChild(newElement2); 
+        
+        
+        dispayFilter.innerHTML = "No registration for the selected town";
+        return;
+      }
+
     }
 
   }
 }
 
-function clearAll(){
-  localStorage ['reg'] = "";
+function clearAll() {
+  localStorage['reg'] = "";
   storePlatesLocal = [];
   dispayElement.innerHTML = "";
   dispayFilter.innerHTML = "";
